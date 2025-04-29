a64e8ca6b89d8b2d3d23154cb26d9e326db0adf7
shree256 <shreeshan256@gmail.com>
2025-04-29 19:48:42 +0530

    Adds setup configs

---
+++ setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="django-easy-logging",
+    version="0.1.0",
+    author="shree256",
+    description="A Django package for easy CRUD operation logging and container logs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shree256/django-easy-logging",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Framework :: Django",
+        "Framework :: Django :: 3.2",
+        "Framework :: Django :: 4.0",
+        "Framework :: Django :: 4.1",
+        "Framework :: Django :: 4.2",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "Django>=3.2",
+    ],
+    extras_require={
+        "dev": [
+            "pytest>=6.0",
+            "pytest-django>=4.1.0",
+        ],
+    },
+)
